3bb4f2cf4412bd8bc680dd5998318f34bd99d748
Fayssal Developer <58825362+mekki14@users.noreply.github.com>
2024-09-08 23:25:45 +0100

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,15 +1,15 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        name: 'Mekki Fayssal',
+        username: 'fayssal',
+        url: 'fayssalmekki.online',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Web developer intersting on serving modern web solution. ',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
+        subdesc: 'Contact me for more information',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
